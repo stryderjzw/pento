317de9ff3d340621b2eb2514ba859286e2498557
Justin Wong <stryderjzw@gmail.com>
2024-01-23 16:18:45 -0800

    remove unused alias

--- lib/pento/search.ex
+++ lib/pento/search.ex
@@ -1,6 +1,5 @@
 defmodule Pento.Search do
   alias Pento.Search.SearchBySKU
-  alias Pento.Catalog
 
   def change_query_by_sku(%SearchBySKU{} = query, attrs \\ %{}) do
     SearchBySKU.changeset(query, attrs)
