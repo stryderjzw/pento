27ddf199d66cf77b86c2419e60f095f187b71b97
Justin Wong <stryderjzw@gmail.com>
2024-02-02 21:00:17 -0800

    add products_with_average_ratings to the survey results live component

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -1,3 +1,17 @@
 defmodule PentoWeb.Admin.SurveyResultsLive do
   use PentoWeb, :live_component
+
+  alias Pento.Catalog
+
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_products_with_average_ratings()}
+  end
+
+  defp assign_products_with_average_ratings(socket) do
+    socket
+    |> assign(:products_with_average_ratings, Catalog.list_products_with_average_ratings())
+  end
 end
