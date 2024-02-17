609d6c3e117d8662efc9d658e2d88d46ddca3280
Justin Wong <stryderjzw@gmail.com>
2024-02-16 18:03:51 -0800

    admin dashboard should listen to presence

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -2,13 +2,15 @@ defmodule PentoWeb.Admin.DashboardLive do
   use PentoWeb, :live_view
 
   alias PentoWeb.Endpoint
-  alias PentoWeb.Admin.SurveyResultsLive
+  alias PentoWeb.Admin.{SurveyResultsLive, UserActivityLive}
 
   @survey_results_topic "survey_results"
+  @user_activity_topic "user_activity"
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
       Endpoint.subscribe(@survey_results_topic)
+      Endpoint.subscribe(@user_activity_topic)
     end
 
     {:ok,
@@ -19,6 +21,15 @@ defmodule PentoWeb.Admin.DashboardLive do
 
   def handle_info(%{event: "rating_created"}, socket) do
     send_update(SurveyResultsLive, id: socket.assigns.survey_results_component_id)
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id
+    )
+
     {:noreply, socket}
   end
 end
